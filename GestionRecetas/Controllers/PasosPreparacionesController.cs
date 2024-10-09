30500ebe48d31dcba168c30974cc340a9db2106e
Eame-prog <erikamarroquin.adoc@outlook.com>
2024-10-09 11:45:37 -0600

    Creacion API

---
+++ GestionRecetas/Controllers/PasosPreparacionesController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GestionRecetas.Models;
+
+namespace GestionRecetas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PasosPreparacionesController : ControllerBase
+    {
+        private readonly RecetasDBContext _context;
+
+        public PasosPreparacionesController(RecetasDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/PasosPreparaciones
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PasosPreparacion>>> GetPasosPreparaciones()
+        {
+            return await _context.PasosPreparaciones.ToListAsync();
+        }
+
+        // GET: api/PasosPreparaciones/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PasosPreparacion>> GetPasosPreparacion(int id)
+        {
+            var pasosPreparacion = await _context.PasosPreparaciones.FindAsync(id);
+
+            if (pasosPreparacion == null)
+            {
+                return NotFound();
+            }
+
+            return pasosPreparacion;
+        }
+
+        // PUT: api/PasosPreparaciones/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPasosPreparacion(int id, PasosPreparacion pasosPreparacion)
+        {
+            var receta = await _context.Recetas.FindAsync(pasosPreparacion.RecetaId);
+            if (receta == null)
+            {
+                return NotFound();
+            }
+
+            pasosPreparacion.Receta = receta;
+
+            if (id != pasosPreparacion.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(pasosPreparacion).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PasosPreparacionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/PasosPreparaciones
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<PasosPreparacion>> PostPasosPreparacion(PasosPreparacion pasosPreparacion)
+        {
+            var receta = await _context.Recetas.FindAsync(pasosPreparacion.RecetaId);
+            if (receta == null)
+            {
+                return NotFound();
+            }
+
+            pasosPreparacion.Receta = receta;
+
+            _context.PasosPreparaciones.Add(pasosPreparacion);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPasosPreparacion", new { id = pasosPreparacion.ID }, pasosPreparacion);
+        }
+
+        // DELETE: api/PasosPreparaciones/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePasosPreparacion(int id)
+        {
+            var pasosPreparacion = await _context.PasosPreparaciones.FindAsync(id);
+            if (pasosPreparacion == null)
+            {
+                return NotFound();
+            }
+
+            _context.PasosPreparaciones.Remove(pasosPreparacion);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PasosPreparacionExists(int id)
+        {
+            return _context.PasosPreparaciones.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ GestionRecetas/Models/Receta.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GestionRecetas.Models
+{
+    public class Receta
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "El nombre debe tener entre 3 y 100 caracteres")]
+        [Display(Name = "Nombre de producto")]
+        public string NombreReceta { get; set; } = string.Empty;
+
+        [Display(Name = "Descripción de Receta")]
+        public string DescripcionReceta { get; set; } = string.Empty;
+
+        [Display(Name = "Tiempo estimado de preparación")]
+        public string TiempoReceta { get; set; } = string.Empty;
+
+    }
+}
